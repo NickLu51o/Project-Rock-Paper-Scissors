c367423bbc205f8ef81e8076a8704398ed8e2b56
NickLu51o <purextrillest@gmail.com>
2023-04-02 08:50:14 -0700

    created the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-let nameOne = "Nick";
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random()*3)
+    switch (randomNumber) {
+    case 0:
+        return "rock";
+    case 1:
+        return "paper";
+    case 2:
+        return "scissors";
+    }
+}
+
+
+console.log(getComputerChoice())
