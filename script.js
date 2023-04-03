6d7e3e3560c1e6a6678a8a64f30b10a245a88450
NickLu51o <purextrillest@gmail.com>
2023-04-03 10:35:18 -0700

    added playRound function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let playerScore=0
+let computerScore=0
+let roundEnding = " "
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random()*3)
     switch (randomNumber) {
@@ -10,5 +14,34 @@ function getComputerChoice() {
     }
 }
 
+let playerSelection = "rock"
 
-console.log(getComputerChoice())
+function playRound(computerSelection, playerSelection) {
+
+    if ( (playerSelection == "rock" && computerSelection == "paper") ||  
+    (playerSelection == "paper" && computerSelection == "scissors") ||  
+    (playerSelection == "scissors" && computerSelection == "rock") ) {
+    roundEnding = "You Lose!";
+    computerScore++ }
+
+    if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper") ) {
+    roundEnding = "You Win!"
+    playerScore++ }
+
+    if (playerSelection == computerSelection ) {
+    roundEnding = "It's a Tie!" }
+
+    console.log(computerSelection)
+
+    return roundEnding
+
+}
+console.log(playRound(getComputerChoice(), playerSelection))
+
+function GameOver() {
+    playerScore == 5 || computerScore == 5
+    alert ("GameOver")
+}
